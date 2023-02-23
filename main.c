eed0f17c526c1c4e505d61547b71b4c9cf09c7c9
onjegv <vgejno@student.42lausanne.ch>
2023-02-23 19:37:57 +0100

    makefile

--- main.c
+++ main.c
@@ -6,16 +6,32 @@
 /*   By: vgejno <vgejno@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 11:28:22 by vgejno            #+#    #+#             */
-/*   Updated: 2023/02/23 11:33:17 by vgejno           ###   ########.fr       */
+/*   Updated: 2023/02/23 19:36:43 by vgejno           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main()
+/*1.initialise reserved_words, envp, envp_list
+2.1.while(!exit_flag): readline, ft_readline?, add to history if there is a line
+2.2.module: lexer: divides str in tokens. 
+2.3.with lexer get info out of tokens, divide them into input, output, args, 
+type ocommands: pipeline or standart commends..
+2.4.execution: when we know everything about command, launch it, wait to execute,
+exit
+3.destruct: free all resources
+*/
+int	main(void)
 {
-	char *str;
-	
-	while((str = readline("prompt> ")))
+	char	*str;
+
+	str = readline("prompt> ");
+	while (str) //if the line is not empty it has to be added to history
+	{
 		printf("%s\n", str);
+		if (str)
+			add_history(str);
+		// return (str);	
+	}
+	return (0);	
 }
--- minishell.h
+++ minishell.h
@@ -6,15 +6,39 @@
 /*   By: vgejno <vgejno@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 11:29:01 by vgejno            #+#    #+#             */
-/*   Updated: 2023/02/23 11:47:58 by vgejno           ###   ########.fr       */
+/*   Updated: 2023/02/23 19:35:38 by vgejno           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef		MINISHELL_H
-# define	MINISHELL_H
+#ifndef MINISHELL_H
+# define MINISHELL_H
 
 # include <stdio.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+# include <sys/wait.h>
+
+// typedef int(*t_builtin_ptr)(t_llist *, t_info *);
+
+typedef struct s_info
+{
+	// t_builtin_ptr	builtin[7];
+	char			reserved_words[7];
+	char			**envp;
+	// struct s_list	envp_list;
+	char			envp_flag;
+	char			exit_flag;
+	int				status;
+	
+}t_info;
+
+typedef struct s_list
+{
+	void			*key;
+	void			*value;
+	struct s_list	*next;
+}t_list;
+
+
 
 #endif
